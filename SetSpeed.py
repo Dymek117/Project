ab45620e5c5d702bcd897393411a50805fb778f5
Damian <dymek117@gmail.com>
2017-04-12 12:39:49 +0200

    Software for new PWM Driver controlling v0.04

--- SetSpeed.py
+++ SetSpeed.py
@@ -36,58 +36,25 @@ pwm_address = 0b00111110   # A6,A5 TO GND, A4-A0 TO VCC
 # CONFIG REGISTERS
 
 mode_1_reg_address = 0x00     # MODE1 REGISTER
-mode_2_reg_address = 0x01     # MODE2 REGISTER
-
-mode_1_reg_value = 0x00       # JUST CLEAR
-mode_2_reg_value = 0x00       # GROUP DIMMING
+mode_1_reg_value = 0x00       # ENABLE OSCILLATOR
 
 
 # MOTOR ENABLE REGISTERS ADDRESS
 
-north_east_address = 0x0D    # 1st, TOP ROW,    PWM 4
-north_west_address = 0x0D    # 2nd, TOP ROW,    PWM 5
-west_address =       0x0C    # 3rd, TOP ROW,    PWM 3
-south_west_address = 0x0C    # 1st, BOTTOM ROW, PWM 2
-south_east_address = 0x0C    # 2nd, BOTTOM ROW, PWM 1
-east_address =       0x0C    # 3rd, BOTTOM ROW, PWM 0
+PWM_ON_ADDR_LOW = [0x06, 0x0A, 0x0E, 0x12, 0x16, 0x1A]
+PWM_ON_ADDR_HIGH = [0x07, 0x0B, 0x0F, 0x13, 0x17, 0x1B]
 
 
-# MOTOR ALL-ENABLE REGISTER VALUES [ WRITE TO BOTH LEDOUT REGISTERS! ]
+# MOTOR DISABLE REGISTERS ADDRESS
 
-motors_on_0 = 0b01010101    # PWM0, PWM1, PWM2, PWM3
-motors_on_1 = 0b00000101    # PWM4, PWM5
-
-
-# MOTOR DISABLE VALUES [ USE 'AND' OPERATOR ONLY! ]
-
-north_east_off = 0b11111100   # PWM 4, LEDOUT1
-north_west_off = 0b11110011   # PWM 5, LEDOUT1
-west_off =       0b00111111   # PWM 3, LEDOUT0
-south_west_off = 0b11001111   # PWM 2, LEDOUT0
-south_east_off = 0b11111001   # PWM 1, LEDOUT0
-east_off =       0b11111100   # PWM 0, LEDOUT0
+PWM_OFF_ADDR_LOW = [0x08, 0x0C, 0x10, 0x14, 0x18, 0x1C]
+PWM_OFF_ADDR_HIGH = [0x09, 0x0D, 0x11, 0x15, 0x19, 0x1D]
 
 
 # DISABLE DIMMING
 
 dimming_address      = 0x0A    # GRPPWM
 dimming_value        = 0xFF    # 99.6% [ 0.005223s ]
-
-
-# WORKING VARIABLES
-
-timer_list = [0] * 6
-
-
-# TASK POINTERS
-
-NE_task = 0
-NW_task = 0
-W_task = 0
-SW_task = 0
-SE_task = 0
-E_task = 0
-scheduler = 0
 
 
 # ====================================================================
@@ -107,9 +74,26 @@ scheduler = 0
 # ESTABILISH I2C CONNECTION, DRIVER CONFIGURATION
 
 def start_pwm_driver(pwm_addr):
+    
     pwm_id = wiringpi2.wiringPiI2CSetup(pwm_addr)
     time.sleep(0.001)
+    
+    # OSCILLATOR = ON 
     wiringpi2.wiringPiI2CWriteReg8(pwm_id, mode_1_reg_address, mode_1_reg_value)
+
+    # FREQ = 200Hz        
+    wiringpi2.wiringPiI2CWriteReg8(pwm_id, 0xFE, 0x1E)
+    
+    # SET MOTORS ENABLE DELAY TIME = 100
+    for a in range(0, 6):
+        
+        PWM_MSB, PWM_LSB = divmod(100, 1<<8)
+        
+        byte_low = PWM_ON_ADDR_LOW[a]
+        byte_high = PWM_ON_ADDR_HIGH[a]
+        
+        wiringpi2.wiringPiI2CWriteReg8(pwm_id, byte_low, PWM_LSB)
+        wiringpi2.wiringPiI2CWriteReg8(pwm_id, byte_high, PWM_MSB)
     
     return pwm_id
 
@@ -122,66 +106,22 @@ def start_pwm_driver(pwm_addr):
 def set_time(control_list):
 
     for a in range(0, 6):
-        timer_list[a] = (control_list[a] + 100) * 0.00001
+        timer_list[a] = int(919 + (819 * control_list[a]/100))
 
     return timer_list
 
 
 
-# TURN ON ALL MOTORS
-
-def all_motors_on(pwm_id):
-
-    wiringpi2.wiringPiI2CWriteReg8(pwm_id, 0x0C, 0b01010101)
-    wiringpi2.wiringPiI2CWriteReg8(pwm_id, 0x0D, 0b00000101)
-
-
-
-# START TIMER FOR AXIS TURN OFF
-
-def start_timer(pwm_id, address, value):
-
-    wiringpi2.wiringPiI2CWriteReg8(pwm_id, address, value)
-
-
-
 # TURN ON MOTORS, SET TASKS FOR EVERY AXIS ENABLE TIME, TRIGGER IN 200Hz LOOP!
 
-def scheduler_start(pwm_id, times):
+def set_times(pwm_id, times):
 
-    all_motors_on(pwm_id) # LOGIC '1' FOR EVERY CHANNEL
-
-    # SCHEDULE TASKS FOR TURNING OFF EACH CHANNEL
-
-    NE_task = Timer(times[0], start_timer, args = (pwm_id, north_east_address, north_east_off))
-    NW_task = Timer(times[1], start_timer, args = (pwm_id, north_west_address, north_west_off))
-    W_task = Timer(times[2], start_timer, args = (pwm_id, west_address, west_off))
-    SW_task = Timer(times[3], start_timer, args = (pwm_id, south_west_address, south_west_off))
-    SE_task = Timer(times[4], start_timer, args = (pwm_id, south_east_address, south_east_off))
-    E_task = Timer(times[5], start_timer, args = (pwm_id, east_address, east_off))
-
-    scheduler = Timer(0.005, scheduler_start, args = (pwm_id, timer_list))        # REPEAT AFTER 5ms
-
-    NE_task.start()
-    NW_task.start()
-    W_task.start()
-    SW_task.start()
-    SE_task.start()
-    E_task.start()
-
-    scheduler.start()
-
-
-# STOP ALL TASKS, BREAK PWM DRIVER COMMUNICATION
-
-def scheduler_stop():
-
-    NE_task.cancel()
-    NW_task.cancel()
-    W_task.cancel()
-    SW_task.cancel()
-    SE_task.cancel()
-    E_task.cancel()
-
-    scheduler.cancel()
-
+    for a in range(0, 6):
+        
+        PWM_MSB, PWM_LSB = divmod(times[a], 1<<8)
+        
+        byte_low = PWM_OFF_ADDR_LOW[a]
+        byte_high = PWM_0FF_ADDR_HIGH[a]
+        
+        wiringpi2.wiringPiI2CWriteReg8(pwm_id, byte_low, PWM_LSB)
+        wiringpi2.wiringPiI2CWriteReg8(pwm_id, byte_high, PWM_MSB)
